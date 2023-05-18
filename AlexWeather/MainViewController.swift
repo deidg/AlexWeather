76a3414ba646f6dc9b9ec4d092e975a5cf234220
deidg <64797316+deidg@users.noreply.github.com>
2023-05-18 13:20:32 +0300

    commit 1.0.3 3 labels done(MVP)

--- AlexWeather/MainViewController.swift
+++ AlexWeather/MainViewController.swift
@@ -12,13 +12,26 @@ class MainViewController: UIViewController {
     let gradientLayer = CAGradientLayer()
     var topColor = UIColor.orange
     var bottomColor = UIColor.yellow
+    let temperatureLabel: UILabel = {
+        let temperatureLabel = UILabel()
+        temperatureLabel.backgroundColor = .red
+        return temperatureLabel
+    }()
+    let conditionsLabel: UILabel = {
+        let conditionsLabel = UILabel()
+        conditionsLabel.backgroundColor = .green
+        return conditionsLabel
+    }()
+    let locationLabel: UILabel = {
+        let locationLabel = UILabel()
+        locationLabel.backgroundColor = .white
+        return locationLabel
+    }()
     
-
+    
     let theStoneImageView = UIImageView(image: UIImage(named: "image_stone_cracks.png"))
-//    theStoneImageView.sizeToFit()
-
-//    остановился на добавлении камня на картинку.
-
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +39,7 @@ class MainViewController: UIViewController {
         configureGradientLayer()
         setupUI()
     }
-   
+    
     func configureGradientLayer() {
         gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
         gradientLayer.locations = [0,1]
@@ -39,6 +52,27 @@ class MainViewController: UIViewController {
         theStoneImageView.snp.makeConstraints { make in
             make.top.equalTo(view.snp.top)
             make.centerX.equalTo(self.view)
+        }
+        view.addSubview(temperatureLabel)
+        temperatureLabel.snp.makeConstraints{ make in
+            make.top.equalTo(theStoneImageView.snp.bottom).inset(10)
+            make.leading.equalToSuperview().inset(20)
+            make.trailing.equalToSuperview().inset(270)
+            make.height.equalTo(100)
+        }
+        view.addSubview(conditionsLabel)
+        conditionsLabel.snp.makeConstraints{ make in
+            make.top.equalTo(temperatureLabel.snp.bottom).offset(10)
+            make.leading.equalToSuperview().inset(20)
+            make.trailing.equalToSuperview().inset(250)
+            make.height.equalTo(50)
+        }
+        view.addSubview(locationLabel)
+        locationLabel.snp.makeConstraints{ make in
+            make.centerX.equalTo(self.view)
+            make.bottom.equalTo(view.snp.bottom).inset(50)
+            make.leading.trailing.equalToSuperview().inset(100)
+            make.height.equalTo(50)
         }
     }
 }
