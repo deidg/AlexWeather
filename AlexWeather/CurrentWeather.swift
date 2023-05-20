cdcc94eb8bc058e34c48a840b2d9a088b7f9c573
deidg <64797316+deidg@users.noreply.github.com>
2023-05-20 13:40:58 +0300

    commit 1.0.16 parsed data to let

---
+++ AlexWeather/CurrentWeather.swift
@@ -0,0 +1,25 @@
+//
+//  CurrentWeather.swift
+//  AlexWeather
+//
+//  Created by Alex on 20.05.2023.
+//
+
+import Foundation
+
+struct CurrentWeather {
+    let cityName: String  //  name?
+    
+    let temperature: Double
+    var temperatureString: String {
+        return "\(temperature.rounded())"
+    }
+    
+    let description: String
+
+    init?(currentWeatherData: CurrentWeatherData) {
+        cityName = currentWeatherData.name
+        temperature = currentWeatherData.main.temp
+        description = currentWeatherData.name.description
+    }
+}
--- AlexWeather/NetworkManager.swift
+++ AlexWeather/NetworkManager.swift
@@ -16,22 +16,27 @@ struct NetworkManager {
         let session = URLSession(configuration: .default)
         let task = session.dataTask(with: url) { data, response, error in
             if let data = data {
-                parseJSON(withData: data)  //.self?
+                let currentWeather = parseJSON(withData: data)  //.self?
             }
         }
         task.resume()
     }
 }
 
-func parseJSON(withData data: Data) {
+func parseJSON(withData data: Data) -> CurrentWeather? {
     let decoder = JSONDecoder()
     do {
         let currentWeatherData = try decoder.decode(CurrentWeatherData.self, from: data)
-        print(currentWeatherData)
+
+        guard let currentWeather = CurrentWeather(currentWeatherData: currentWeatherData)
+        else {
+            return nil
+        }
+        return currentWeather
     } catch let error as NSError {
         print(error.localizedDescription)
     }
-   
+   return nil
 }
 
 
