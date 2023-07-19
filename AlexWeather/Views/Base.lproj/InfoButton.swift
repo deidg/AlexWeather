269b8d766b422ba14eb2cf7b4a41b80b93fd2f45
deidg <64797316+deidg@users.noreply.github.com>
2023-07-19 13:34:41 +0300

    commit 1.2.114 infoButton refactored

--- AlexWeather/Views/Base.lproj/InfoButton.swift
+++ AlexWeather/Views/Base.lproj/InfoButton.swift
@@ -10,28 +10,40 @@ import UIKit
 class InfoButton: UIButton {
     private var topColor = Constants.topColor
     private var bottomColor = Constants.bottomColor
-    private let infoButtonGradientLayer = CAGradientLayer()
+    private let infoButtonGradientLayer: CAGradientLayer = {
+        let infoButtonGradientLayer = CAGradientLayer()
+//        infoButtonGradientLayer.frame = bounds
+        return infoButtonGradientLayer
+    }()
  
     override init(frame: CGRect) {
         super.init(frame: .zero)
+        
+        setupUI()
+        setupLayers()
+    }
+    override func layoutSubviews() {
+            super.layoutSubviews()
+            infoButtonGradientLayer.frame = bounds //не убирать! или вкл 42. Кнопка жёлтая
+        }
+    required init?(coder: NSCoder) {
+        return nil
+    }
+    
+    func setupUI() {
         setTitle("INFO", for: .normal)
         setTitleColor(.black, for: .normal)
         contentVerticalAlignment = .top
-        
         clipsToBounds = true
         layer.cornerRadius = Constants.cornerRadius
-
+    }
+    
+    func setupLayers() {
         infoButtonGradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
         infoButtonGradientLayer.locations = [0,1]
         self.layer.addSublayer(infoButtonGradientLayer)
         infoButtonGradientLayer.frame = self.bounds
-    }
-    override func layoutSubviews() {
-            super.layoutSubviews()
-            infoButtonGradientLayer.frame = bounds
-        }
-    required init?(coder: NSCoder) {
-        return nil
+//        infoButtonGradientLayer.frame = bounds
     }
 }
 
